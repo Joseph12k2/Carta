81a73f6178ad223ea54c9b5e05cc859697f034d6
Joseph12k2 <joseph17sasha@gmail.com>
2025-02-14 15:29:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const btnOpenElement = document.querySelector('#open');
+const btnCloseElement = document.querySelector('#close');
+
+btnCloseElement.disabled = true;
+
+btnOpenElement.addEventListener('click', () => {
+  btnOpenElement.disabled = true;
+  btnCloseElement.disabled = false;
+  const coverElement = document.querySelector('.cover');
+  coverElement.classList.add('open-cover');
+
+  setTimeout(() => {
+    coverElement.style.zIndex = -1;
+
+    const paperElement = document.querySelector('.paper');
+    paperElement.classList.remove('close-paper');
+    paperElement.classList.add('open-paper');
+
+    // 🔹 Asegura que el scroll esté habilitado
+    paperElement.style.overflowY = 'auto';
+    paperElement.scrollTop = 0;
+
+    const heartElement = document.querySelector('.heart');
+    heartElement.style.display = 'block';
+  }, 500);
+});
+
+btnCloseElement.addEventListener('click', () => {
+  btnOpenElement.disabled = false;
+  btnCloseElement.disabled = true;
+
+  const coverElement = document.querySelector('.cover');
+  const paperElement = document.querySelector('.paper');
+  paperElement.classList.remove('open-paper');
+  paperElement.classList.add('close-paper');
+
+  setTimeout(() => {
+    coverElement.style.zIndex = 1;
+    coverElement.classList.remove('open-cover');
+
+    const heartElement = document.querySelector('.heart');
+    heartElement.style.display = 'none';
+  }, 500);
+});
